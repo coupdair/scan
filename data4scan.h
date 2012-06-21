666f47725422d61a3e511a39fcbe723a197932ae
Sebastien COUDERT <sebastien.coudert@univ-lille1.fr>
2012-06-21 22:51:33 +0200

    add flag in NetCDF. modified:   Makefile: upgrade version to v0.1.7 . modified:   VERSION: v0.1.7 . modified:   data4scan.h: add both flag NetCDF names and write to NetCDF. modified:   scan.help.output: auto updated.

--- data4scan.h
+++ data4scan.h
@@ -34,6 +34,13 @@ public:
 ///scan (copy in scan)
   cimg_library::CImg<Tmap> flag;//3D (X,Y,Z) 0 then red=0, green=1
   cimg_library::CImg<Tmap> fail;//3D (X,Y,Z) 0 then increment++
+#ifdef cimg_use_netcdf
+  //! map ad fail information: 
+  std::string flag_name;
+  std::string flag_unit_name;
+  std::string fail_name;
+  std::string fail_unit_name;
+#endif //cimg_use_netcdf
 
   //! constructor
   /**
@@ -53,7 +60,7 @@ std::cerr<<this->class_name<<"::"<<__func__<<"()\n"<<std::flush;
     ///set variable name
     this->component_name.clear();
     this->component_name.push_back("intensity");
-    this->unit_name.push_back("none");
+    this->unit_name.push_back("gray level");
     ///set dimendion names
     this->dimension_name.clear();
     this->dimension_name.push_back("x");//image width
@@ -61,6 +68,11 @@ std::cerr<<this->class_name<<"::"<<__func__<<"()\n"<<std::flush;
     this->dimension_name.push_back("X");//scanning width
     this->dimension_name.push_back("Y");//scanning height
     this->dimension_name.push_back("Z");//scanning depth
+    //! map ad fail information: 
+    flag_name="flag";
+    flag_unit_name="none; 0=outlier, 1=validated";
+    fail_name="fail_counter";
+    fail_unit_name="count";
 #endif //cimg_use_netcdf
   }//constructor
 
@@ -149,39 +161,25 @@ std::cerr<<this->class_name<<"::"<<__func__<<"("<<file_path<<")\n"<<std::flush;
     dim_names.push_back(this->dimension_name[2]);//z
     dim_names.push_back(this->dimension_name[3]);//v
     string     dim_time=this->dimension_name[4]; //l
-      //variable names
-/** /
-      ///single
-      string var_name="flag";
-      ///list
-      vector<string> var_names;
-      var_names.push_back("gray_level");
-      //unit names
-      ///single
-      string unit_name="none; 0=outlier, 1=validated";
-      ///list
-      vector<string> unit_names;
-      unit_names.push_back("none; range=[0-255]");
-*/
-    CImgNetCDF<float> cimgListTest4D;
-/*
-      //CImgList<int> imgList3D(nZ,nz,nv);//i.e. XYZ, same size list
-      //CImgNetCDF<int> cimgTest2D;
+    //NetCDF for CImg
+    CImgNetCDF<float> cimgListTest4D;//for data: xyXY(Z)
+    CImgList<int> imgList3D(flag.depth);//for flag: XY(Z), same size list
+    CImgNetCDF<int> cimgTest2D;
+    cimglist_for(imgList3D,Z) imgList3D(Z)=flag.get_shared_plane(Z);
 (*this).print("5D");
-//imgList3D.print("3D");
+imgList3D.print("3D");
 /*move to Cdata*/
     ////file
     cout << "CImgNetCDF::saveNetCDFFile(" << file_path << ",...) return " 	<< cimgListTest4D.saveNetCDFFile((char*)file_path.c_str()) << endl;
-    //cout << "CImgNetCDF::setNetCDFFile(" << file_path << ",...) return " 	<< cimgTest2D.setNetCDFFile(cimgListTest4D.pNCFile) << endl;
+    cout << "CImgNetCDF::setNetCDFFile(" << file_path << ",...) return " 	<< cimgTest2D.setNetCDFFile(cimgListTest4D.pNCFile) << endl;
     ////dim
     cout << "CImgNetCDF::addNetCDFDims(" << file_path << ",...) return " 	<< cimgListTest4D.addNetCDFDims((*this)[0],dim_names,dim_time) << endl;
-    //std::vector<NcDim*>vpNCDim;
-    //vpNCDim.push_back(cimgListTest4D.vpNCDim[2]);//X
-    //vpNCDim.push_back(cimgListTest4D.vpNCDim[3]);//Y
-    //cout << "CImgNetCDF::setNetCDFDims(" << file_path << ",...) return " 	<< cimgTest2D.setNetCDFDims(vpNCDim,cimgListTest4D.pNCDimt) << endl;
+    std::vector<NcDim*>vpNCDim;
+    vpNCDim.push_back(cimgListTest4D.vpNCDim[2]);//X
+    vpNCDim.push_back(cimgListTest4D.vpNCDim[3]);//Y
+    cout << "CImgNetCDF::setNetCDFDims(" << file_path << ",...) return " 	<< cimgTest2D.setNetCDFDims(vpNCDim,cimgListTest4D.pNCDimt) << endl;
     ////var
     cout << "CImgNetCDF::addNetCDFVar(" << file_path << ",...) return " 	<< cimgListTest4D.addNetCDFVar((*this)[0],this->component_name[0],this->unit_name[0]) << endl;
-//! \todo . add maximum position as attribute
     {//maximum position as attribute
     CImg<int> max_position(5);max_position=-1;
     //maximum
@@ -192,12 +190,12 @@ std::cerr<<this->class_name<<"::"<<__func__<<"("<<file_path<<")\n"<<std::flush;
     std::string axis_order="(";for(int i=0;i<this->dimension_name.size()-1;++i) axis_order+=this->dimension_name[i]+","; axis_order+=this->dimension_name[this->dimension_name.size()-1]+")";
     cimgListTest4D.pNCvar[0].add_att("maximum_position_order",axis_order.size(),axis_order.c_str());
     }//maximum position as attribute
-    //cout << "CImgNetCDF::addNetCDFVar(" << file_path << ",...) return " 	<< cimgTest2D.addNetCDFVar(imgList3D[0],var_name,unit_name) << endl;
+    cout << "CImgNetCDF::addNetCDFVar(" << file_path << ",...) return " 	<< cimgTest2D.addNetCDFVar(imgList3D[0],flag_name,flag_unit_name) << endl;
     ////data
     cimglist_for((*this),l)
     {
       cout << "CImgNetCDF::addNetCDFData(" << file_path << ",...) return " 	<< cimgListTest4D.addNetCDFData((*this)[l]) << endl;
-    //  cout << "CImgNetCDF::addNetCDFData(" << file_path << ",...) return " 	<< cimgTest2D.addNetCDFData(imgList3D[l],cimgListTest4D.loadDimTime()-1) << endl;
+      cout << "CImgNetCDF::addNetCDFData(" << file_path << ",...) return " 	<< cimgTest2D.addNetCDFData(imgList3D[l],cimgListTest4D.loadDimTime()-1) << endl;
     }
     return true;
   }//save_NetCDF
